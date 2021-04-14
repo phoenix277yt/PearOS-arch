03c85c58d17f5bfbb87e69d3e0717d2351947963
Matt C <mcompton2002@gmail.com>
2021-04-14 18:30:38 -0400

    WIP plasma ISO

--- peariso/build_iso.sh
+++ peariso/build_iso.sh
@@ -1,5 +1,7 @@
+rm -fv pearos-live-*.iso
+
 WORKDIR=$(mktemp -d)
 # idk if this would've happened automatically?
 cp pear/pacman.conf pear/airootfs/etc/.
-sudo mkarchiso -v -w $WORKDIR -o . pear
+time sudo ./mkarchiso -v -w $WORKDIR -o . pear
 sudo rm -rf $WORKDIR
--- peariso/pear/profiledef.sh
+++ peariso/pear/profiledef.sh
@@ -13,5 +13,6 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
+  ["/etc/shadow"]="0:0:0400"
+  ["/etc/gshadow"]="0:0:0400"
 )
