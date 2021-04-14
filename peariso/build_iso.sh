e207a75df66da77659e7a31f9f49619632396131
Matt C <mcompton2002@gmail.com>
2021-04-14 15:09:21 -0400

    fix build script

--- peariso/build_iso.sh
+++ peariso/build_iso.sh
@@ -1,9 +1,5 @@
-if [ -d WORKDIR]; then
-    sudo rm -rf WORKDIR
-fi
-
+WORKDIR=$(mktemp -d)
 # idk if this would've happened automatically?
 cp pear/pacman.conf pear/airootfs/etc/.
-mkdir WORKDIR
-sudo mkarchiso -v -w WORKDIR -o . pear
-sudo rm -rf WORKDIR
+sudo mkarchiso -v -w $WORKDIR -o . pear
+sudo rm -rf $WORKDIR
