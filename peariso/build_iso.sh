c85e5285117ede52a3b1e11dd462657662f0e77a
Matt C <mcompton2002@gmail.com>
2021-04-16 10:34:21 -0400

    so i had a misunderstanding of cala

--- peariso/build_iso.sh
+++ peariso/build_iso.sh
@@ -3,6 +3,7 @@ rm -fv pearos-live-*.iso
 WORKDIR=$(mktemp -d)
 # idk if this would've happened automatically?
 cp pear/pacman.conf pear/airootfs/etc/.
+echo "Built on $(date +"%D @ %T EST")" >> pear/airootfs/etc/buildstamp
 time sudo ./mkarchiso -v -w $WORKDIR -o . pear
 sudo rm -rf $WORKDIR
 
