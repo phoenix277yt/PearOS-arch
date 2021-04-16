5330d68662db7b4d0b6220156b526758035f1b05
Matt C <mcompton2002@gmail.com>
2021-04-16 12:30:21 -0400

    Lord maybe sqfs?

--- peariso/build_iso.sh
+++ peariso/build_iso.sh
@@ -1,9 +1,19 @@
 rm -fv pearos-live-*.iso
 
+if [[ ! -f pear/airootfs/sysroot.sqfs ]]; then
+    if [[ ! -d sysroot ]]; then
+        mkdir sysroot
+    fi
+    sudo pacstrap sysroot $(cat sysrootpkgs)
+    mksquashfs sysroot pear/airootfs/sysroot.sqfs
+    read
+fi
+
+
 WORKDIR=$(mktemp -d)
 # idk if this would've happened automatically?
 cp pear/pacman.conf pear/airootfs/etc/.
-echo "Built on $(date +"%D @ %T EST")" >> pear/airootfs/etc/buildstamp
+echo "Built on $(date +"%D @ %T EST")" > pear/airootfs/etc/buildstamp
 time sudo ./mkarchiso -v -w $WORKDIR -o . pear
 sudo rm -rf $WORKDIR
 
--- peariso/chrooted.sh
+++ peariso/chrooted.sh
@@ -5,7 +5,8 @@ echo "Performing minor tweaks"
 sed -i 's/Arch/PearOS/g' /etc/issue
 sed -i 's/Arch/PearOS/g' /etc/arch-release
 sed -i 's/Arch/PearOS/g' /etc/os-release
-echo "PearOS-Live" > /etc/hostname
+echo "PearLive" > /etc/hostname
+
 # We don't add carly until here so that our packages which change
 # /etc/skel have been installed already
 /usr/bin/useradd -m carly
