501480eac7e66fba5d5209a4fd90333fece49c3c
phoenix <73348506+phoenix277yt@users.noreply.github.com>
2021-04-17 20:00:42 +0530

    broken script pls fix, couldnt get it to work.

---
+++ jumpstart.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#untested pls fix if error
+sudo pacman -Syyu --needed base-devel make cmake linux-headers vi vim nano python tk yay
+sudo pacman -S gambas3
+mkdir ~/pearApps
+cd ~/pearApps
+mkdir control-centre
+cd control-centre
+wget https://github.com/alxb421/Control-Centre/releases/download/11.0.1/xyz.pearos.control-centre_11.1.0_all.deb
+ccdebian=$"xyz.pearos.control-centre_11.1.0_all.deb"
+sudo pacman -U fakeroot binutils
+yay -S debtap
+sudo debtap -U
+sudo debtap $ccdebian
+ccalpm=$(echo *.pkg.tar.zst)
+sudo pacman -U $ccalpm
+cd ~/pearApps
+git clone https://github.com/alxb421/pext-installer.git
+cd pext-installer
+sudo mv pext-installer /usr/share
+cd ~/pearApps
+git clone https://github.com/alxb421/piri-backend.git
+cd piri-backend
+sudo chmod +x install.sh
+sh install.sh
+cd ~/pearApps
+mkdir sys-overview
+cd sys-overview
+wget https://github.com/alxb421/system-overview/releases/download/11.0.1/xyz.pearos.system-overview_11.1.0_all.deb
+sudo debtap -U
+ccdebian=$("xyz.pearos.system-overview_11.1.0_all.deb")
+debtap $ccdebian
+ccalpm=$(echo *.pkg.tar.zst)
+sudo pacman -U $ccalpm
+cd ~/pearApps
+mkdir updmgr
+cd updmgr
+wget https://github.com/alxb421/update-mgr/releases/download/11.1/xyz.pearos.update-mgr_11.1_all.deb
+ccdebian=("xyz.pearos.update-mg_11.1_all.deb")
+sudo debtap $ccdebian
+ccalpm=$(echo *.tar.zst)
+sudo pacman -U $ccalpm
